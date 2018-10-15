7dbd9dea91a6c61a859dc4399f8346ebc033a4f8
Quế Hương <luquehuong@gmail.com>
2018-10-15 18:01:23 +0700

    feature: pagination

--- src/components/BeerTable.vue
+++ src/components/BeerTable.vue
@@ -10,10 +10,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr 
-          v-for="beer in sortedList" 
-          :key="beer.id"
-        >
+        <tr v-for="beer in getRows()" :key="beer.id">
           <td><img class="img" :src="beer.image_url"></td>
           <td>{{ beer.name }}</td>
           <td>{{ beer.tagline }}</td>
@@ -21,6 +18,13 @@
         </tr>
       </tbody>
     </table>
+    <div class="pagination">
+      <div class="number"
+        v-for="i in numPages()" :key="i.id"
+        :class="[i == currentPage ? 'active' : '']"
+        @click="changePage(i)">{{i}}
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -32,6 +36,8 @@ export default {
       list: [],
       currentSort:'',
       currentSortDir:'',
+      currentPage: 1,
+      elementsPerPage: 10,
     };
   },
   async created() {
@@ -49,6 +55,17 @@ export default {
         this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
       }
       this.currentSort = s;
+    },
+    numPages: function numPages() {
+      return Math.ceil(this.list.length / this.elementsPerPage);
+    },
+    getRows: function getRows() {
+      var start = (this.currentPage-1) * this.elementsPerPage;
+      var end = start + this.elementsPerPage;
+      return this.sortedList.slice(start, end);
+    },
+    changePage: function changePage(page) {
+      this.currentPage = page;
     }
   },
   computed:{
@@ -65,17 +82,14 @@ export default {
 }
 </script>
 <style scoped>
-
 img {
   width: 20px;
   height: 70px;
 }
-
 table {
   font-family: 'Open Sans', sans-serif;
   width: 100%;
 }
-
 table th {
   width: 25%;
   padding: 1rem;
@@ -98,6 +112,24 @@ table td:last-child {
 }
 table tbody tr:nth-child(2n) td {
   background: #e5f1f9;
+}
+.pagination {
+  font-family: 'Open Sans', sans-serif;
+  text-align: right;
+  width: 750px;
+  padding: 8px;
+}
+.number {
+  display: inline-block;
+  padding: 4px 10px;
+  color: #FFF;
+  border-radius: 4px;
+  background: #2980b9;
+  margin: 0px 5px;
+  cursor: pointer;
+}
+.number:hover, .number.active {
+  background: #96f293;
 }
 </style>
 
